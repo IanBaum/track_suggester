c1f8d593a810bc8e0bd1ed13211700c5e0f97b33
Ian Baum <ian@ianbaum.com>
2016-08-12 13:30:46 -0400

    update css with new colors and styles

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
   var trackIndex = 0;
   var appIndex = 0;
 
+
+
   var questions = ["#userNameDiv","#question1","#question2","#question3","#question4","#question5",]
   var tracks = ["C#/.NET", "CSS/Design", "Java/Android", "PHP/Drupal", "Ruby/Rails"]
   var apps = ["in house buisness software", "user interfaces for websites and apps", "android and enterprise apps", "content heavy websites", "interactive websites"]
@@ -20,7 +22,8 @@ $(document).ready(function(){
     if(questionIndex === questions.length -1){
      $("#nextBtn").hide();
      $("#doneBtn").show();
-   }
+     $("nameError").hide();
+    }
   });
 
   // Prev button
@@ -48,27 +51,26 @@ $(document).ready(function(){
     var interactiveData = parseInt($("#interactiveData").val());
     var dataManage = parseInt($("#dataManage").val());
     var location = parseInt($("#location").val())
-    if(location < 2){ //if location is not philly
-      if(softOrWeb === 0){ //if software check for mobile or desktop
-        if(desktopOrMobile === 0){ //if desktop set track to C#
-          trackIndex = 0;
-        }else if(desktopOrMobile === 0){ //if mobile or both set track to Java
-          trackIndex = 2;
-        }else{
-          trackIndex = 2;
+      if(location < 2){ //if location is not philly
+        if(softOrWeb === 0){ //if software check for mobile or desktop
+          if(desktopOrMobile === 0){ //if desktop set track to C#
+            trackIndex = 0;
+          }else if(desktopOrMobile === 0){ //if mobile or both set track to Java
+            trackIndex = 2;
+          }else{
+            trackIndex = 2;
+          }
+        }else if(frontOrBack === 0){ // if website and frontend set CSS
+          trackIndex = 1;
+        }else if(interactiveData === 0){ //if websitea and interactive set Ruby
+          trackIndex = 4;
+        }else{ //otherwise choose php
+          trackIndex = 3;
         }
-      }else if(frontOrBack === 0){ // if website and frontend set CSS
-        trackIndex = 1;
-      }else if(interactiveData === 0){ //if websitea and interactive set Ruby
-        trackIndex = 4;
-      }else{ //otherwise choose php
+      }else{ //if location is philly
         trackIndex = 3;
       }
-    }else{ //if location is philly
-      trackIndex = 3;
-    }
-
-    var name = $("#userName").val().toLowerCase().replace(/\b[a-z]/g, function(letter){return letter.toUpperCase();});
+      var name = $("#userName").val().toLowerCase().replace(/\b[a-z]/g, function(letter){return letter.toUpperCase();});
     var language = tracks[trackIndex];
     var app = apps[trackIndex];
 
@@ -82,6 +84,9 @@ $(document).ready(function(){
   // Reset button
   $("#resetBtn").click(function(event){
     event.preventDefault();
+    history.go(0);
+  });
+  $("#resetNav").click(function(){
     history.go(0);
   });
 });
