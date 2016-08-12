516622e5faa25d3fe6ba2f661ca5c50652047af8
Ian Baum <ian@ianbaum.com>
2016-08-12 10:11:22 -0400

    add UI and navigation logic for question interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,44 @@
 $(document).ready(function(){
 
+  var questionIndex = 0;
+
+  var  questions = ["#userNameDiv","#userTestDiv"]
+
+  // Next button
+  $("#nextBtn").click(function(event){
+    event.preventDefault();
+    if(questionIndex < questions.length - 1){
+      $(questions[questionIndex]).hide();
+      questionIndex++;
+      $(questions[questionIndex]).show();
+      $("#prevBtn").show();
+    }
+    if(questionIndex === questions.length -1){
+     $("#nextBtn").hide();
+     $("#doneBtn").show();
+   }
+  });
+
+  // Prev button
+  $("#prevBtn").click(function(event){
+    event.preventDefault();
+    if(questionIndex >= 1){
+      $(questions[questionIndex]).hide();
+      questionIndex--;
+      $(questions[questionIndex]).show();
+      $("#nextBtn").show();
+      $("#doneBtn").hide();
+    }
+    if(questionIndex === 0){
+     $("#prevBtn").hide();
+   }
+  });
+  
+  // Done button
+  $("#userInfo").submit(function(event){
+    event.preventDefault();
+    var name = $("#userName").val();
+
+    console.log(name);
+  });
 });
